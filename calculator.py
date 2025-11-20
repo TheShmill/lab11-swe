d044c3f02eeb156da50038d872ef75a2ed343471
TheShmill <21013808+TheShmill@users.noreply.github.com>
2025-11-20 12:13:49 -0500

    Implemented exp function

--- calculator.py
+++ calculator.py
@@ -4,15 +4,25 @@ calculator.py
 
 One function per operation, in order.
 """
+
+
 # First example
-def add(a, b): 
+def add(a, b):
     return a + b
+
 
 def sub(a, b):
     return a - b
 
+
 def multilpy(a, b):
     return a * b
 
+
 def divide(a, b):
     return a / b
+
+
+def exponent(a, b):
+    return pow(a, b)
+
